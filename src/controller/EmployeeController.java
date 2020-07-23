62ee773f0f50d0de5118c270597c677ac8d1d66e
tharinduperera <tharindudananjaya199818@gmail.com>
2020-07-23 17:29:34 +0530

    design emplyee form & create employee controller

---
+++ src/controller/EmployeeController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class EmployeeController {
+
+
+    public AnchorPane root;
+    public TextField txtid;
+    public TextField txtname;
+    public Button btnSave;
+    public Button btnRemove;
+    public TextField txtsalary;
+    public TableView tblEmployee;
+    public TextField txtEtf;
+
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnRemoveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtsalaryOnAction(ActionEvent actionEvent) {
+    }
+}
